fda982d4c8217ae3563f9c1e531438e0059963b8
avilaeugeneb <avila.eugeneb@gmail.com>
2018-04-11 15:35:16 +0800

    Added home content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+/*************************/
+/**                     **/
+/** Darken Nav Script   **/
+/**                     **/
+/*************************/
+
+
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar-fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+/********************************/
+/**                     	   **/
+/** Show More/Show Less Toggle **/
+/**                            **/
+/********************************/
+
