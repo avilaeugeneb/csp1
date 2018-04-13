4ca9d5a353d683e40963099b9ae49fed76cfaa0d
avilaeugeneb <avila.eugeneb@gmail.com>
2018-04-13 10:24:43 +0800

    Feat : added 'Go to top' button, deleted todo.txt

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,26 @@ $(function () {
 
 /********************************/
 /**                     	   **/
-/** Show More/Show Less Toggle **/
+/**     Go to top Button       **/
 /**                            **/
 /********************************/
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnbot-top").style.display = "block";
+    } else {
+        document.getElementById("btnbot-top").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+     $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+
+}
